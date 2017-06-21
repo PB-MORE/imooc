ba9b38c9b8a7a4d98ae78bda1e14c350373da766
PB-MORE <PBMORE@163.com>
2017-06-21 17:12:20 +0800

    node+express+MongoDB建站

--- app.js
+++ app.js
@@ -1,135 +1,151 @@
-var express = require('express')
-var bodyParser=require('body-parser');
-var serveStatic=require('serve-static');
-var path = require('path')
-var mongoose = require('mongoose')
-var _=require('underscore')
-var Movie = require('./models/movie')
-var port =process.env.PORT || 3000
-var app =express()
+var express = require('express');  // 加载express模块
+var app = express(); // 启动Web服务器
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/imooc')
+var port = process.env.PORT || 3000; // 设置端口号：3000
+app.listen(port); // 监听 port[3000]端口
+console.log('i_movie start on port' + port);
 
-app.set('views','./views/pages')
-app.set('view engine','jade')
-app.use(bodyParser.urlencoded());
-app.use(serveStatic('public'));
-app.locals.moment = require('moment');
-app.listen(port)
+var path = require('path');
+// 引入path模块的作用：因为页面样式的路径放在了bower_components，告诉express，请求页面里所过来的请求中，如果有请求样式或脚本，都让他们去bower_components中去查找
 
-console.log('imooc started on port ' + port);
+var mongoose = require('mongoose'); // 加载mongoose模块
+mongoose.connect('mongodb://localhost:27017/imovie'); // 连接mongodb本地数据库imovie
+console.log('MongoDB connection success!');
+/*  mongoose 简要知识点补充
+* mongoose模块构建在mongodb之上，提供了Schema[模式]、Model[模型]和Document[文档]对象，用起来更为方便。
+* Schema对象定义文档的结构（类似表结构），可以定义字段和类型、唯一性、索引和验证。
+* Model对象表示集合中的所有文档。
+* Document对象作为集合中的单个文档的表示。
+* mongoose还有Query和Aggregate对象，Query实现查询，Aggregate实现聚合。
+* */
 
-//index page
-app.get('/',function(req,res){
+app.locals.moment = require('moment'); // 载入moment模块，格式化日期
 
-	Movie.fetch(function(err,movies){
-		if(err){
-			console.log(err);
-		}
-		res.render('index',{
-			title:'imooc 首页',
-			movies:movies
-		})
-	})
-})
+var serveStatic = require('serve-static');  // 静态文件处理
+app.use(serveStatic('public')); // 路径：public
 
-		//detail page
-app.get('/movie/:id',function(req,res){
-	var id=req.params.id;
+var bodyParser = require('body-parser');
+// 因为后台录入页有提交表单的步骤，故加载此模块方法（bodyParser模块来做文件解析），将表单里的数据进行格式化
+app.use(bodyParser.urlencoded({extended: true}));
 
-	Movie.findById(id,function(err,movie){
-		res.render('detail',{
-		title:'imooc '  + movie.title,
-		movie: movie
-		})
-	})
-	
-})
+var _underscore = require('underscore'); // _.extend用新对象里的字段替换老的字段
 
-		//admin page
-app.get('/admin/movie',function(req,res){
-	res.render('admin',{
-		title:'imooc 后台录入页',
-		movie:{
-			title: ' ',
-			doctor: ' ',
-			country: ' ',
-			year: ' ',
-			language: ' ',
-			summary: ' ',
-			poster: ' ',
-			flash: ' '
-		}
-	})
-})
-		//admin update movie
-app.get('/admin/update/:id',function(req,res){
-	var id = req.params.id
-	if(id){
-		Movie.findById(id,function(err,movie){
-			res.render('admin',{
-			title:'imooc 后台更新页',
-				movie:movie
-			})
-		})
-	}
-})
-		//admin post movie
-app.post('/admin/movie/new',function(req,res){
-	var id = req.body.movie._id
-		var movieObj = req.body.movie
-		var _movie
-		if(id !== 'undefined'){
-			Movie.findById(id,function(err,movie){
-				if(err){
-					console.log(err)
-				}
-				_movie = _.extend(movie,movieObj)
-				_movie.save(function(err,movie){
-					if(err){
-						console.log(err)
-					}
-					res.redirect('/movie/'+movie._id)
-				})
-			})
-		}else{
-			_movie = new Movie({
-				doctor:movieObj.doctor,
-				title:movieObj.title,
-				country:movieObj.country,
-				language:movieObj.language,
-				year:movieObj.year,
-				poster:movieObj.poster,
-				summary:movieObj.summary,
-				flash:movieObj.flash
-			})
-			_movie.save(function(err,movie){
-				if(err){
-					console.log(err)
-				}
-				res.redirect('/movie/'+movie._id)
-			})
-		}
-})
+app.set('views', './views/pages');     // 设置视图默认的文件路径
+app.set('view engine', 'jade');  // 设置视图引擎：jade
 
-		//list page
-app.get('/admin/list',function(req,res){
-	Movie.fetch(function(err,movies){
-		if(err){
-			console.log(err);
-		}
-		res.render('list',{
-			title:'imooc 列表页',
-			movies:movies
-		})
-	})
-})
-	//list delete movie
+var movie = require('./models/movie.js'); // 载入mongoose编译后的模型movie
+
+// 编写主要页面路由
+// index page 首页
+app.get('/', function (req, res) {
+    movie.fetch(function (err, movies) {
+        if (err) {
+            console.log(err);
+        }
+        res.render('index', {  // 渲染index 首页
+            title: 'imooc 首页',
+            movies: movies
+        });
+    });
+});
+
+// detail page 详情页
+app.get('/movie/:id', function (req, res) {
+    var id = req.params.id;
+    movie.findById(id, function (err, movie) {
+        res.render('detail', {
+            title: 'imooc' + movie.title,
+            movie: movie
+        });
+    });
+});
+
+// admin page 后台录入页
+app.get('/admin/movie', function (req, res) {
+    res.render('admin', {
+        title: 'imooc 后台录入页',
+        movie: {
+            title: '',
+            doctor: '',
+            country: '',
+            year: '',
+            poster: '',
+            flash: '',
+            summary: '',
+            language: ''
+        }
+    });
+});
+
+// admin update movie 后台更新页
+app.get('/admin/update/:id', function (req, res) {
+    var id = req.params.id;
+    if (id) {
+        movie.findById(id, function (err, movie) {
+            res.render('admin', {
+                title: 'imooc 后台更新页',
+                movie: movie
+            });
+        });
+    }
+});
+
+// admin post movie 后台录入提交
+app.post('/admin/movie/new', function (req, res) {
+    var id = req.body.movie._id;
+    var movieObj = req.body.movie;
+    var _movie = null;
+    if (id !== 'undefined') { // 已经存在的电影数据
+        movie.findById(id, function (err, movie) {
+            if (err) {
+                console.log(err);
+            }
+            _movie = _underscore.extend(movie, movieObj); // 用新对象里的字段替换老的字段
+            _movie.save(function (err, movie) {
+                if (err) {
+                    console.log(err);
+                }
+                res.redirect('/movie/' + movie._id);
+            });
+        });
+    } else {  // 新加的电影
+        _movie = new movie({
+            doctor: movieObj.doctor,
+            title: movieObj.title,
+            country: movieObj.country,
+            language: movieObj.language,
+            year: movieObj.year,
+            poster: movieObj.poster,
+            summary: movieObj.summary,
+            flash: movieObj.flash
+        });
+        _movie.save(function (err, movie) {
+            if (err) {
+                console.log(err);
+            }
+            res.redirect('/movie/' + movie._id);
+        });
+    }
+});
+
+// list page 列表页
+app.get('/admin/list', function (req, res) {
+    movie.fetch(function (err, movies) {
+        if (err) {
+            console.log(err);
+        }
+        res.render('list', {
+            title: 'imooc 列表页',
+            movies: movies
+        });
+    });
+});
+
+// list delete movie data 列表页删除电影
 app.delete('/admin/list', function (req, res) {
     var id = req.query.id;
     if (id) {
-        Movie.remove({_id: id}, function (err, movie) {
+        movie.remove({_id: id}, function (err, movie) {
             if (err) {
                 console.log(err);
             } else {
